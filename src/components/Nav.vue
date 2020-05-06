2f5af5ae3a08e8b809d083f6835fa6053395ba6f
TracyChen33 <tracychen3621@gmail.com>
2020-05-06 16:23:04 -0600

    Nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-link to="/money">Money</router-link>
+    |
+    <router-link to="/labels">Labels</router-link>
+    |
+    <router-link to="/statistics">Statistics</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Nav extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Sorry,the page does not exist,Please check your web link</div>
+    <div>
+      <router-link to="/">Back to home page</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NotFound extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
