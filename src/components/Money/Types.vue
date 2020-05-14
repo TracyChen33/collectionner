1c79f1d3f6e3c1c544cf9e8d544565c2c4e2316b
TracyChen33 <tracychen3621@gmail.com>
2020-05-13 18:19:33 -0600

    modularization for Money.vue

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul class="types">
+      <li class="selected">Spend</li>
+      <li>Income</li>
+    </ul>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .types {
+    background: #6699cc;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333333;
+      }
+    }
+  }
+
+
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,56 +1,23 @@
 <template>
   <Layout class-prefix="layout">
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>Delete</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>Clear</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">OK</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">Spend</li>
-        <li>Income</li>
-      </ul>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="Please enter the remark">
-      </label>
-    </div>
-    <div class="tags">
-      <div class="new">
-        <button>New Label</button>
-      </div>
-      <ul class="current">
-        <li>food</li>
-        <li>cloth</li>
-        <li>home</li>
-        <li>walk</li>
-      </ul>
-    </div>
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
   </Layout>
-
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Tags from '@/components/Money/Tags.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Types from '@/components/Money/Types.vue';
 
-  @Component({})
+  @Component({
+    components: {Types, Notes, Tags, NumberPad}
+  })
   export default class Money extends Vue {
 
   }
@@ -61,150 +28,4 @@
     display: flex;
     flex-direction: column-reverse;
   }
-</style>
-<style lang="scss" scoped>
-  @import "~@/assets/style/helper.scss";
-  @import "~@/assets/style/reset.scss";
-
-  .numberPad {
-    .output {
-      @extend %innerShadow;
-      font-size: 36px;
-      font-family: Consolas, monospace;
-      padding: 9px 16px;
-      text-align: right;
-    }
-
-    .buttons {
-      @extend %clearFix;
-
-      > button {
-        width: 25%;
-        height: 64px;
-        float: left;
-        background: transparent;
-        border: none;
-
-        &.ok {
-          height: 64*2px;
-          float: right;
-        }
-
-        &.zero {
-          width: 25*2%;
-        }
-
-        $bg: #99ccff;
-
-        &:nth-child(1) {
-          background: $bg;
-        }
-
-        &:nth-child(2), &:nth-child(5) {
-          background: darken($bg, 6%);
-        }
-
-        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
-          background: darken($bg, 6*2%);
-        }
-
-        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
-          background: darken($bg, 6*3%);
-        }
-
-        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
-          background: darken($bg, 6*4%);
-        }
-
-        &:nth-child(14) {
-          background: darken($bg, 6*5%);
-        }
-
-        &:nth-child(12) {
-          background: darken($bg, 6*6%);
-        }
-      }
-    }
-  }
-
-  .types {
-    background: #6699cc;
-    display: flex;
-    text-align: center;
-    font-size: 24px;
-
-    > li {
-      width: 50%;
-      height: 64px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-
-      &.selected::after {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 4px;
-        background: #333333;
-      }
-    }
-  }
-
-  .notes {
-    background: #f5f5f5;
-    font-size: 14px;
-    display: flex;
-    padding-left: 16px;
-    align-items: center;
-
-    .name {
-      padding-right: 16px;
-    }
-
-    input {
-      height: 64px;
-      flex-grow: 1;
-      background: transparent;
-      border: none;
-      padding-right: 16px;
-    }
-  }
-
-  .tags {
-    font-size: 14px;
-    padding: 16px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column-reverse;
-
-    > .current {
-      display: flex;
-flex-wrap: wrap;
-      > li {
-        background: #99ccff;
-        $h: 24px;
-        height: $h;
-        line-height: $h;
-        border-radius: $h/2;
-        padding: 0 16px;
-        margin-right: 12px;
-      }
-    }
-
-    > .new {
-      padding-top: 16px;
-
-      button {
-        background: transparent;
-        border: none;
-        color: #999;
-        border-bottom: 1px solid;
-        padding: 0 4px;
-      }
-    }
-  }
-
 </style>
