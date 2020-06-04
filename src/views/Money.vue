0abdb16b06b0438183400ade21b5b7314ba74f16
TracyChen33 <tracychen3621@gmail.com>
2020-06-04 17:18:11 -0600

    model.ts and custom.d.ts for localStore with MVC thinking partten

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,12 @@
 <template>
   <Layout class-prefix="layout">
+    {{recordList}}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
+
 </template>
 
 <script lang="ts">
@@ -14,24 +16,18 @@
   import Tags from '@/components/Money/Tags.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Types from '@/components/Money/Types.vue';
+  import model from '@/model';
 
-  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') ||'[]');
+  const recordList = model.fetch();
 
-  type Record = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
-    creatAt?: Date;
-  }
 
   @Component({
     components: {Types, Notes, Tags, NumberPad}
   })
   export default class Money extends Vue {
     tags = ['Food', 'Cloth', 'Work', 'Home'];
-    recordList: Record[] = recordList;
-    record: Record = {tags: [], notes: '', type: '-', amount: 0};
+    recordList: RecordItem[] = recordList;
+    record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
     onUpdateTags(value: string[]) {
       this.record.tags = value;
@@ -42,14 +38,14 @@
     }
 
     saveRecord() {
-      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      const record2: RecordItem = model.clone(this.record);
       record2.creatAt = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+      model.save(this.recordList);
     }
   }
 </script>
