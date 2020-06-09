82e45d2e2ae9193d139ae13d28d69e7daef4371d
TracyChen33 <tracychen3621@gmail.com>
2020-06-09 00:01:13 -0600

    router for EditLabel.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,6 +1,9 @@
 <template>
   <Layout>
-    <div>EditLabel</div>
+    <div>
+      <Icon name="left" />
+     <span>EditLabel</span>
+    </div>
   </Layout>
 
 </template>
@@ -8,9 +11,24 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import tagListModel from '@/models/tagListModel';
 
   @Component
   export default class EditLabel extends Vue {
+    created(){
+      const id =this.$route.params.id;
+      tagListModel.fetch;
+      const tags = tagListModel.data;
+      const tag = tags.filter(t=>t.id === id);
+      if(tag){
+        console.log(tag)
+      }else{
+        this.$router.replace('/404')
+      }
+
+
+    }
+
 
   }
 </script>
